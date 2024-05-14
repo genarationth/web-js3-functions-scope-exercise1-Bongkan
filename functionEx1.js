af9a98ff0b65ae166b3c628c168abe90eab29e3e
Bongkan <bongkan.jj@gmail.com>
2024-05-14 13:53:07 +0700

    Finished

---
+++ functionEx1.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function dataMe(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + "I live in " + location + " and enjoy " + hobby);
+}
+
+console.log(add(3, 5));
+console.log(subtract(3, 5));
+console.log(multiply(3, 5));
+console.log(divide(3, 5));
+dataMe("Bongkan", "Lamphun", "Sleeping");
+
